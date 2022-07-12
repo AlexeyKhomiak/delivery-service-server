642618ea0efef1aa45d8b2d8ffc58963c3d0a629
AlexeyKhomiak <alexey.khomiak@gmail.com>
2022-07-12 16:36:04 +0300

    fix cors7

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://fierce-wave-79346.herokuapp.com/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use("/api", orderRouter);
 app.use("/api", cartRouter);
 app.use("/api", shopRouter);
