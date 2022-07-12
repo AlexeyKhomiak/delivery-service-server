503135e79a47170aebe1ceae5788537b9ae305eb
AlexeyKhomiak <alexey.khomiak@gmail.com>
2022-07-12 13:24:02 +0300

    Added product.amount in db query

--- controller/shop.controller.js
+++ controller/shop.controller.js
@@ -9,7 +9,7 @@ class ShopController {
 
     async getShopWithProductsById(req, res) {
         const id = req.params.id;
-        const shops = await db.query("SELECT shop.id as shop_id, shop.name as shop_name, product.id, product.name, product.price, product.shop_id, product.img FROM shop, product WHERE shop.id = product.shop_id AND shop.id = $1", [id]);
+        const shops = await db.query("SELECT shop.id as shop_id, shop.name as shop_name, product.id, product.name, product.price, product.shop_id, product.img, product.amount FROM shop, product WHERE shop.id = product.shop_id AND shop.id = $1", [id]);
         res.json(shops.rows);
     }
 
