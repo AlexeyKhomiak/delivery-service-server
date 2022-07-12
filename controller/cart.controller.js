7a44672f150236e562f345345526e030cf438096
AlexeyKhomiak <alexey.khomiak@gmail.com>
2022-07-12 15:56:24 +0300

    fix cors5

--- controller/cart.controller.js
+++ controller/cart.controller.js
@@ -6,12 +6,14 @@ class CartController {
         const {orderId, prodId} = req.body;
         const newCart = await db.query("INSERT INTO order_prod (order_id, prod_id) VALUES ($1, $2) RETURNING *",
             [orderId, prodId]);
+        res.set('Access-Control-Allow-Origin', '*');
         res.json(newCart.rows);        
     }
 
     async getCartByOrder(req, res) {
         const id = req.query.id;
         const cart = await db.query("SELECT * FROM order_prod WHERE order_id = $1", [id]);
+        res.set('Access-Control-Allow-Origin', '*');
         res.json(cart.rows);
     }
 }
